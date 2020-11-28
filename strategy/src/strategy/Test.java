9723b74de2c03365f225f0ef27de773930586905
Safaena123 <safaenabet7@gmail.com>
2020-11-28 12:10:29 +0100

    Creating the array of objects

--- strategy/src/strategy/Test.java
+++ strategy/src/strategy/Test.java
@@ -6,21 +6,21 @@ public class Test {
 		
 		// we can provide any strategy to do the sorting 
 		
-		ArrayList<Product> list = new ArrayList<Product>();
-		list.add(new Product("book", 150, 200)); 
-        list.add(new Product("pencil", 5, 10)); 
-        list.add(new Product("laptop", 10000, 10000)); 
-        list.add(new Product("arduino", 1000, 2000));
+		Product product[] = new Product[4];
+		product[0] = new Product("book", 150, 200);
+		product[1] = new Product("pen", 10, 20);
+		product[2] = new Product("laptop", 15000, 20000);
+		product[3] = new Product("arduino", 1500, 2000);
         
         Context ctx = new Context(new BubbleSort());
-        ctx.arrange(list);
+        ctx.arrange(product);
         
         // we can change the strategy without changing Context class
         ctx = new Context(new QuickSort());
-        ctx.arrange(list);
+        ctx.arrange(product);
 
         System.out.println("Products after sorting : "); 
-        for (Product product: list) 
+        for (Product product: product) 
         { 
             System.out.println(product.getName() + " " + 
                                product.getPrice() + " " + 
@@ -30,6 +30,10 @@ public class Test {
 	// try it yourself then …
 
 
+	}
+
+	private static void extracted(Product[] product) {
+		product[0] = new Product("book", 150, 200);
 	}
 
 }
